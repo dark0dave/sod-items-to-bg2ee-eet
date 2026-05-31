d112abcc9e4736fb45bca046098a0cb88913d8c7
dark0dave <dark0dave@mykolab.com>
2026-05-31 12:45:14 +0100

    feat(components): seperate into components
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- scripts/check-ie.sh
+++ scripts/check-ie.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
 function checkIE() {
-  declare -i count=0
   declare -i badfiles=0
 
-  for file in $(find k0_iskp/. -type f -iname "${1}" ); do
+  for file in $(find ./k0_iskp/ -iname "*.${1}" ); do
     [[ $( basename "${file}" | cut -f1 -d '.' | wc | awk '{ print $3}' ) -gt 9 ]] && echo "Error found: ${file} which is larger than 8 characters" && badfiles+=1;
-    count+=1;
   done
 
-  [[ "${badfiles}" -gt 0 ]] && echo "Failed found bad files" && exit 1;
+  if [[ "${badfiles}" -gt 0 ]]; then
+    echo "Failed found bad files";
+    exit 1;
+  fi
+}
 
-  echo "Checked ${count} files"
+function checkPathsInTp2() {
+  for file in $(find . -type f -iname "*.tp2" | sort | uniq ); do
+    for fileToMove in $(grep -E "COPY\W~.*\..{3}~\W~override" "${file}" | sed -E "s/^.*~(.*)~\W~.*$/\1/g"); do
+      if [[ ! -f "${fileToMove}" ]]; then
+        echo "${fileToMove} is not a file";
+        exit 1;
+      fi
+    done
+  done
+}
+
+function uniqueDesignated() {
+  for file in $(find . -type f -iname "*.tp2" | sort | uniq ); do
+    duplicates=$(grep -E "DESIGNATED\W+[0-9]+" "${file}" | sort | uniq -d);
+    if [[ ! -z "${duplicates}" ]]; then
+      echo "${duplicates} is duplicated in ${file}";
+      exit 1;
+    fi
+  done
 }
 
 main() {
@@ -25,6 +46,8 @@ main() {
   checkIE "*sto"
   checkIE "*wav"
   checkIE "*vvc"
+  checkPathsInTp2
+  uniqueDesignated
 }
 
 main
