091a4d2539eda4ea2f531079c2980df479521ba8
dark0dave <dark0dave@mykolab.com>
2026-06-01 06:53:17 +0100

    feat(files): seperate files for components
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- k0_iskp/dlg/k0trgen.d
+++ k0_iskp/dlg/k0trgen.d
@@ -1,18 +1,12 @@
 EXTEND_BOTTOM TRGENI01 35
-
-IF ~Global("BD_HAVE_SOD","GLOBAL",1)
-    Global("K0_SOD_IMPORT_30","GLOBAL",1)~
-
-THEN DO ~GiveItemCreate("bdsw1h08",LastTalkedToBy,0,0,0)
-         SetGlobal ("K0_SOD_IMPORT_30","GLOBAL",2)~ GOTO 36
+  IF ~Global("K0_ITEMS_BDSW1H08","GLOBAL",0)~
+    THEN DO ~GiveItemCreate("bdsw1h08",LastTalkedToBy,0,0,0)
+    SetGlobal ("K0_ITEMS_BDSW1H08","GLOBAL",1)~ GOTO 36
 END
 
 EXTEND_BOTTOM TRGENI01 39
-
-IF ~Global("BD_HAVE_SOD","GLOBAL",1)
-    Global("K0_SOD_IMPORT_30","GLOBAL",1)~
-
-THEN DO ~TakePartyItem("misc8k")
-         GiveItemCreate("bdsw1h08",LastTalkedToBy,0,0,0)
-         SetGlobal ("K0_SOD_IMPORT_30","GLOBAL",2)~ GOTO 36
+  IF ~Global("K0_ITEMS_BDSW1H08","GLOBAL",0)~
+    THEN DO ~TakePartyItem("misc8k")
+    GiveItemCreate("bdsw1h08",LastTalkedToBy,0,0,0)
+    SetGlobal ("K0_ITEMS_BDSW1H08","GLOBAL",1)~ GOTO 36
 END
