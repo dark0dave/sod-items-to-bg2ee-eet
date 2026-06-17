a8dcf97e739ca0069934a164e5b3c50f90bd122f
dark0dave <dark0dave@mykolab.com>
2026-06-17 22:01:37 +0100

    fix(hk): hk fix
    Signed-off-by: dark0dave <dark0dave@mykolab.com>

--- scripts/check-weidu.sh
+++ scripts/check-weidu.sh
@@ -1,25 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-validate() {
+main() {
   echo $1
   extension=$( echo "${1}" | awk -F '.' '{print $NF}' )
   weidu --nogame --parse-check "${extension}" "${1}"
 }
 
-main() {
-  for file in $( find . -type f -iname "*.d" ); do
-    validate "${file}"
-  done
-  for file in $( find . -type f -iname "*.tp2" ); do
-    validate "${file}"
-  done
-  for file in $( find . -type f -iname "*.tpa" ); do
-    validate "${file}"
-  done
-  for file in $( find . -type f -iname "*.tpp" ); do
-    validate "${file}"
-  done
-}
-
-main
+main $@
